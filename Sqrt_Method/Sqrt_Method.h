96cbf92dc0de09369bedbb7ec4792b7340f2c16d
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2018-10-15 22:05:40 +0300

    Added square root method

--- Sqrt_Method/Sqrt_Method.h
+++ Sqrt_Method/Sqrt_Method.h
@@ -6,12 +6,27 @@ using namespace std;
 
 class Sqrt_Method: public Method {
 
-
+	Matrix<double> S;
+	Vector<double> y;
 
 public:
 
 	Sqrt_Method(size_t);
 
 	void solve(ostream&) override;
+
+	void calculateS();
+
+	void solveForY();
+
+	void solveForX();
+
+	double determinant() override;
+
+	void findInverse();
+
+	void printInverse(ostream& out) override;
+
+	void printInverseDeficiency(ostream&) override;
 
 };
